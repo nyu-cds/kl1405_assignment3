c6979ab93802614464b7790976f454c107536071
kl1405 <kliu@KaiwendeMacBook-Pro.local>
2017-05-07 20:13:30 -0400

    assignment 13

--- product_spark.py
+++ product_spark.py
@@ -3,20 +3,13 @@
 from pyspark import SparkContext
 from operator import mul
 
-
 if __name__ == '__main__':
-
 	sc = SparkContext("local", "product")
-
 	# Create an RDD of numbers from 1 to 1,000
 
-    nums = sc.parallelize(range(1, 1001))
+	nums = sc.parallelize(range(1, 1001))
+	#Use fold method to creat a program that calculates the product of all the numbers from 1 to 1000 	
+	#and prints the result.   # https://spark.apache.org/docs/1.1.1/api/python/pyspark.rdd.RDD-class.html#fold product = nums.fold(1, mul)
 
-    #Use fold method to creat a program that calculates the product of all the numbers from 1 to 1000 
-	#and prints the result.
-    # https://spark.apache.org/docs/1.1.1/api/python/pyspark.rdd.RDD-class.html#fold
-
-    product = nums.fold(1, mul)
-
-    print(product)
+	print(product)
 
